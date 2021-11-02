84d261f5a80569b5f83dc553705b48273a32665a
Pavel <98765432123456789r@gmail.com>
2021-11-03 02:51:26 +0300

    Created view

---
+++ AssemblyBrowserApplication/ViewModel.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using AssemblyBrowserApplication.Annotations;
+using Library;
+using Microsoft.Win32;
+
+namespace AssemblyBrowserApplication
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        public void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private readonly AssemblyLoader AssemblyLoader = new AssemblyLoader();
+        public List<Namespace> Namespaces { get; private set; }
+
+        private string _assemblyFilePath;
+
+        public string AssemblyFilePath
+        {
+            get => _assemblyFilePath;
+            set
+            {
+                _assemblyFilePath = value;
+                try
+                {
+                    Namespaces = AssemblyLoader.GetAssemblyInfo(_assemblyFilePath);
+                }
+                catch (FileNotFoundException e)
+                {
+                    Console.Error.WriteLine($"File {_assemblyFilePath} not found");
+                    _assemblyFilePath = null;
+                }
+                OnPropertyChanged(nameof(Namespaces));
+            }
+        }
+
+        public ICommand OpenFileCommand => new OpenFileCommand(OpenAssembly);
+
+        private void OpenAssembly()
+        {
+            var openFileDialog = new OpenFileDialog
+            {
+                Filter = "Assemblies|*.dll",
+                Title = "Select assembly",
+                Multiselect = false
+            };
+            bool opened = openFileDialog.ShowDialog() ?? false;
+            if (opened)
+            {
+                AssemblyFilePath = openFileDialog.FileName;
+                OnPropertyChanged(nameof(AssemblyFilePath));
+            }
+        }
+    }
+}
